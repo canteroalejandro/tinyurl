a7e920001c22b99bba1257215a1270e07ea9e685
Alejandro Lucas Cantero <canteroalejandro@gmail.com>
2020-07-02 15:46:12 -0300

    metodo para la creacion de hash de cada url

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,7 @@ class Url < ApplicationRecord
     validates :url, presence: true, uniqueness: true
     validates :shorturl, presence: true, uniqueness: true
     
+    def create_hash
+        self.shorturl = SecureRandom.urlsafe_base64(10)
+    end
 end
